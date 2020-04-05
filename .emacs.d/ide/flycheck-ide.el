4220cdaccbed8ee3403c8def418f621b57fc750e
ftlio <alen@helloinspire.com>
2020-04-05 02:35:59 -0700

    Remove unused

--- .emacs.d/ide/flycheck-ide.el
+++
@@ -1,8 +0,0 @@
-;; ~/.emacs.d/ide/flycheck-ide.el
-
-(use-package flycheck
-  :ensure t
-  :defer t
-  :config)
-
-(provide 'flycheck-ide)
--- .emacs.d/ide/whitespace-ide.el
+++
@@ -1,8 +0,0 @@
-;; ~/.emacs.d/ide/whitespace-ide.el
-
-(setq-default indent-tabs-mode nil)
-(setq-default tab-width 4)
-(defvaralias 'c-basic-offset 'tab-width)
-(add-hook 'before-save-hook 'delete-trailing-whitespace)
-
-(provide 'whitespace-ide)
